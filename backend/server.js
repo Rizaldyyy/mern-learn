471bef4dbbf628d8f1a8e7ac57b3b83a9012e4f8
_rizaldy <rizaldy@hris-system.com>
2024-01-02 10:50:03 +0800

    add vercel configs

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
+const path = require('path')
 const express = require('express');
 const dotenv = require('dotenv').config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 const colors = require('colors');
 
@@ -19,5 +20,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/goals', require('./routes/goalRoutes'));
 
+if(process.env.NODE_ENV === 'prod'){
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  app.get('*', (req, res) => 
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  )
+} else {
+  app.get('/', (req, res) => res.send('Please set into production mode'))
+}
+
 app.use(errorHandler);
-app.listen(port, () => console.log(`Server started on port ${port}`));
+
+if(port){
+  app.listen(port, () => console.log(`Server started on port ${port}`));
+}
+
+module.exports = app
+
